0b7eb256d8e14f0ec22a079c723ade75f2ee2d72
j1sooo <kjisoo1031@gmail.com>
2025-03-05 17:38:02 +0900

    03-04 1일차 Java 실습

---
+++ src/Chap2/IntegerVariableEx.java
@@ -0,0 +1,12 @@
+package Chap2;
+
+// 정수 타입 변수
+
+public class IntegerVariableEx {
+    public static void main(String[] args) {
+        long long1 = 30L;
+        long long2 = 10000000000L;
+        System.out.println(long1);
+        System.out.println(long2);
+    }
+}
---
+++ src/Chap2/Variable.java
@@ -0,0 +1,16 @@
+package Chap2;
+
+// 변수명 규칙
+
+public class Variable {
+    public static void main(String[] args) {
+        int value = 1;
+        System.out.println(value);
+
+        // int 1value = 0; // 컴파일 오류
+        // int #value = 1; // 컴파일 오류
+        int $value = 1;
+        int _value = 1;
+
+    }
+}
